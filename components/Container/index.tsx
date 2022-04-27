fd226ab935551fa6af972ba14e1e009d6c50646c
HanSeong Lee <jkhanseong@gmail.com>
2022-04-27 20:04:46 +0900

    FIX | Extension of style file

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -1,5 +1,5 @@
 import React, {HTMLAttributes} from 'react';
-import styles from './style.module.css';
+import styles from './style.module.scss';
 import cn from 'classnames';
 
 const Container: React.FC<HTMLAttributes<HTMLDivElement>> = ({ className, children, ...props }) => {

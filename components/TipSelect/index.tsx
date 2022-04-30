485882101d93885bc862a3109b9436bd21d16a6d
HanSeong Lee <jkhanseong@gmail.com>
2022-04-30 19:36:42 +0900

    ADD | TipSelect on desktop

--- components/TipSelect/index.tsx
+++ components/TipSelect/index.tsx
@@ -8,11 +8,12 @@ interface IProps extends HTMLAttributes<HTMLDivElement> {
     tips: number[];
     onSelectChange: (name: string, value: any) => void;
     selected: string;
+    customValue: string | number;
 };
 
 const TipSelect: React.FC<IProps> = ({
                                          name, tips, onSelectChange, className,
-                                         selected, ...props
+                                         customValue, selected, ...props
                                      }) => {
     return (
         <div className={cn(styles.tipSelect, className)}
@@ -32,11 +33,14 @@ const TipSelect: React.FC<IProps> = ({
                     {tip}%
                 </button>
             ))}
-            <Input type={'number'}
+            <Input className={styles.customInput}
+                   type={'number'}
                    placeholder={'Custom'}
                    min={0}
+                   value={customValue}
                    onChange={e => {
                        onSelectChange(name, e.target.value);
+                       onSelectChange('tipCustom', e.target.value);
                        onSelectChange('tipSelected', '');
                    }}
             />

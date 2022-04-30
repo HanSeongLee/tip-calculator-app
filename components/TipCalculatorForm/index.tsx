aca09159c0f897682b7d5aaeb8b3a71748e9714a
HanSeong Lee <jkhanseong@gmail.com>
2022-04-30 19:36:55 +0900

    ADD | TipCalculator on desktop

--- components/TipCalculatorForm/index.tsx
+++ components/TipCalculatorForm/index.tsx
@@ -11,79 +11,102 @@ interface IProps {
     };
     onReset: () => void;
     state: {
-        bill: number;
-        people: number;
-        tip: number;
+        bill: string | number;
+        people: string | number;
+        tip: string | number;
+        tipCustom: string | number;
         tipSelected: string;
     };
 };
 
-const TipCalculatorForm: React.FC<IProps> = ({ onChange, result: {tipAmount, total}, onReset, state: {tipSelected} }) => {
+const TipCalculatorForm: React.FC<IProps> = ({ onChange, result: {tipAmount, total}, onReset, state: {bill, people, tip, tipCustom, tipSelected} }) => {
     return (
         <form className={styles.tipCalculatorForm}>
-            <div className={styles.inputWrapper}>
-                <label>
-                    Bill
-                </label>
-                <Input name={'bill'}
-                       icon={'/icons/icon-dollar.svg'}
-                       type={'number'}
-                       min={0}
-                       onChange={e => onChange(e.target.name, e.target.value)}
-                       placeholder={'0'}
-                />
-            </div>
+            <div className={styles.inputContainer}>
+                <div className={styles.inputWrapper}>
+                    <label>
+                        Bill
+                    </label>
+                    <Input name={'bill'}
+                           icon={'/icons/icon-dollar.svg'}
+                           type={'number'}
+                           min={0}
+                           onChange={e => onChange(e.target.name, e.target.value)}
+                           placeholder={'0'}
+                           value={bill}
+                    />
+                </div>
 
-            <div className={styles.inputWrapper}>
-                <label>
-                    Select Tip %
-                </label>
-                <TipSelect className={styles.tipSelect}
-                           name={'tip'}
-                           tips={[5, 10, 15, 25, 50]}
-                           onSelectChange={onChange}
-                           selected={tipSelected}
-                />
-            </div>
+                <div className={styles.inputWrapper}>
+                    <label>
+                        Select Tip %
+                    </label>
+                    <TipSelect className={styles.tipSelect}
+                               name={'tip'}
+                               tips={[5, 10, 15, 25, 50]}
+                               onSelectChange={onChange}
+                               selected={tipSelected}
+                               customValue={tipCustom}
+                    />
+                </div>
 
-            <div className={styles.inputWrapper}>
-                <label>
-                    Number of People
-                </label>
-                <Input name={'people'}
-                       icon={'/icons/icon-person.svg'}
-                       type={'number'}
-                       min={0}
-                       onChange={e => onChange(e.target.name, e.target.value)}
-                       placeholder={'0'}
-                />
+                <div className={styles.inputWrapper}>
+                    <div className={styles.header}>
+                        <label>
+                            Number of People
+                        </label>
+                        {people !== '' && people == 0 && (
+                            <div className={styles.errorMessage}>
+                                Can't be zero
+                            </div>
+                        )}
+                    </div>
+                    <Input name={'people'}
+                           icon={'/icons/icon-person.svg'}
+                           type={'number'}
+                           min={0}
+                           onChange={e => onChange(e.target.name, e.target.value)}
+                           placeholder={'0'}
+                           value={people}
+                           error={people !== '' && people == 0}
+                    />
+                </div>
             </div>
 
             <div className={styles.resultBox}>
-                <div className={styles.header}>
-                    Tip Amount
-                    <div className={styles.unit}>
-                        / person
+                <div className={styles.resultItem}>
+                    <div className={styles.header}>
+                        Tip Amount
+                        <div className={styles.unit}>
+                            / person
+                        </div>
+                    </div>
+                    <div className={styles.value}>
+                        ${tipAmount.toFixed(2)}
                     </div>
                 </div>
-                <div className={styles.value}>
-                    ${tipAmount.toFixed(2)}
-                </div>
-                <div className={styles.header}>
-                    Total
-                    <div className={styles.unit}>
-                        / person
+                <div className={styles.resultItem}>
+                    <div className={styles.header}>
+                        Total
+                        <div className={styles.unit}>
+                            / person
+                        </div>
+                    </div>
+                    <div className={styles.value}>
+                        ${total.toFixed(2)}
                     </div>
                 </div>
-                <div className={styles.value}>
-                    ${total.toFixed(2)}
+                <div className={styles.buttonContainer}>
+                    <button className={styles.resetButton}
+                            type={'reset'}
+                            onClick={_ => {
+                                onReset();
+                            }}
+                            disabled={!bill && !people && !tip}
+                    >
+                        Reset
+                    </button>
                 </div>
-                <button className={styles.resetButton}
-                        type={'reset'}
-                        onClick={onReset}
-                >
-                    Reset
-                </button>
             </div>
         </form>
     );

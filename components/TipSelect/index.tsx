a4508261b02dfbf132540fdb041955e20f0ca8ca
HanSeong Lee <jkhanseong@gmail.com>
2022-04-30 18:09:46 +0900

    ADD | State and onChange in TipSelect

--- components/TipSelect/index.tsx
+++ components/TipSelect/index.tsx
@@ -1,21 +1,34 @@
-import React, {HTMLAttributes} from 'react';
+import React, {HTMLAttributes, useState} from 'react';
 import styles from './style.module.scss';
 import Input from '../Input';
 import cn from 'classnames';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
+    name: string;
     tips: number[];
+    onSelectChange: (name: string, value: any) => void;
 };
 
-const TipSelect: React.FC<IProps> = ({ tips, className, ...props }) => {
+const TipSelect: React.FC<IProps> = ({
+                                         name, tips, onSelectChange, className,
+                                         ...props
+                                     }) => {
+    const [selected, setSelected] = useState('');
+
     return (
         <div className={cn(styles.tipSelect, className)}
              {...props}
         >
             {tips?.map((tip, index) => (
-                <button className={styles.button}
+                <button className={cn(styles.button, {
+                    [styles.active]: selected === `tip-button-${tip}`,
+                })}
                         type={'button'}
                         key={index}
+                        onClick={_ => {
+                            onSelectChange(name, tip);
+                            setSelected(`tip-button-${tip}`);
+                        }}
                 >
                     {tip}%
                 </button>
@@ -23,6 +36,10 @@ const TipSelect: React.FC<IProps> = ({ tips, className, ...props }) => {
             <Input type={'number'}
                    placeholder={'Custom'}
                    min={0}
+                   onChange={e => {
+                       onSelectChange(name, e.target.value);
+                       setSelected('');
+                   }}
             />
         </div>
     );

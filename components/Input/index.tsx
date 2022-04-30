7972420acfaffbe437ab3d3aa1ee6447228cea53
HanSeong Lee <jkhanseong@gmail.com>
2022-04-30 19:36:32 +0900

    ADD | Input on desktop

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -4,20 +4,30 @@ import cn from 'classnames';
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
     icon?: string;
+    error?: boolean;
 };
 
-const Input: React.FC<IProps> = ({ icon, className, ...props }) => {
+const Input: React.FC<IProps> = ({ icon, className, error, ...props }) => {
     return (
-        <div className={cn(styles.inputWrapper, className)}>
-            <img className={styles.icon}
-                 src={icon}
-                 alt={''}
-            />
+        <div className={cn(styles.inputWrapper, className, {
+            [styles.hasIcon]: icon,
+            [styles.error]: error,
+        })}>
+            {icon && (
+                <img className={styles.icon}
+                     src={icon}
+                     alt={''}
+                />
+            )}
             <input className={styles.input}
                    {...props}
             />
         </div>
     );
+};
+
+Input.defaultProps = {
+    error: false,
 };
 
 export default Input;
